901684b9bef72863be4524848ebdd9f7f30de95a
harry <harry.burchnal@gmail.com>
2018-10-31 00:00:36 +0000

    wired up module to accept HourMinutSecond pipe globally

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,17 @@ import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 import { AppComponent } from './app.component';
 import { RoutingModule } from "./routing.module";
-import { RecentTradeListComponent } from "./recent-trade-list/recent-trade-list.component";
+import { RecentTradeListComponent } from "./trades-analysis/trades-analysis.component";
 import { AngularMaterialModule } from "./angular-material.module";
 import { HeaderComponent } from "./header/header.component";
-import { QtyGroupedByPriceComponent } from './recent-trade-list/qty-by-price/qty-grouped-by-price/qty-grouped-by-price.component';
-import { FetchSymbolComponent } from './recent-trade-list/fetch-symbol/fetch-symbol.component';
-import { QtyByPriceComponent } from './recent-trade-list/qty-by-price/qty-by-price.component';
-import { FetchHistoryComponent } from './recent-trade-list/fetch-history/fetch-history.component';
+import { QtyGroupedByPriceComponent } from './trades-analysis/qty-by-price/qty-grouped-by-price/qty-grouped-by-price.component';
+import { FetchSymbolComponent } from './trades-analysis/fetch-symbol/fetch-symbol.component';
+import { QtyByPriceComponent } from './trades-analysis/qty-by-price/qty-by-price.component';
+import { FetchHistoryComponent } from './trades-analysis/fetch-history/fetch-history.component';
 import { SymbolInputComponent } from './customInputs/symbol-input/symbol-input.component';
 import { ErrorInterceptor } from "./error-interceptor";
 import { ErrorComponent } from "./error/error.component";
+import { HoursMinutesSecondsPipe } from "./hms.pipe";
 
 @NgModule({
   declarations: [
@@ -27,7 +28,8 @@ import { ErrorComponent } from "./error/error.component";
     QtyByPriceComponent,
     FetchHistoryComponent,
     SymbolInputComponent,
-    ErrorComponent
+    ErrorComponent,
+    HoursMinutesSecondsPipe
   ],
   imports: [
     BrowserModule,
